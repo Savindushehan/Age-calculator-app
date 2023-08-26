6afc2f93b62c799e7cbd0e0ae917a4d32022db1d
Savindu Shehan <savindushehansamaraweera@gmail.com>
2023-08-26 12:16:09 +0530

    month change

--- javascript.js
+++ javascript.js
@@ -66,15 +66,24 @@ BTN.onclick=function(){
     d9.style.display="block";
     d3.innerHTML="must be vald date";
     d3.style.display="block";
-   }else if
-   (d8.value==""&&d2.value>31&&d5.value>12)
-   {
+   }else if (d8.value==""&&d2.value>31&&d5.value>12){
     d9.style.display="block";
     d9.style.display="block";
     d3.innerHTML="must be vald date";
     d6.innerHTML="must be valid month";
     d6.style.display="block";
-   }else{
+   }else if(d5.value>month){
+   var y=year-d8.value;
+    var y1=Math.abs(y);
+    var m=((12-d5.value)+month+1);
+    var m1=Math.abs(m);
+    var d=day-d2.value;
+    var dd=Math.abs(d);
+      d10.innerHTML=y1-1;
+      d12.innerHTML=dd;
+      d11.innerHTML=m;
+   }
+   else{
     var y=year-d8.value;
     var y1=Math.abs(y);
     var m=month-d5.value;
